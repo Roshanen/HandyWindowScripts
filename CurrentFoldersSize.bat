5bd49bf55712af248c01d8225f59f31c4f5a21d3
Kachen <106764256+Roshanen@users.noreply.github.com>
2024-08-02 19:08:34 +0700

    Feat: Folders in current directory size

---
+++ CurrentFoldersSize.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+(for /d %%i in (*) do (
+    echo Directory of %%i
+    set lastLine=
+    for /f "tokens=*" %%j in ('dir /s /a "%%i" 2^>nul ^| findstr /c:"File(s)"') do (
+        set lastLine=%%j
+    )
+    if defined lastLine (
+        echo !lastLine!
+    ) else (
+        echo No files found
+    )
+    echo.
+)) > output.txt
+
+echo output.txt file ready
+
+endlocal
